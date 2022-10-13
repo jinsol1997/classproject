02478e6d11fcac5a293f8ffbac11c5f4b12e2c66
jinsol1997 <mngjs97@gmail.com>
2022-10-13 10:56:50 +0900

    Update select_exam1.sql

--- sql/select_exam1.sql
+++ sql/select_exam1.sql
@@ -5,9 +5,9 @@ select ename, sal, sal+300
 from emp;
 
 -- 2. 사원의 이름, 급여, 연간 총 수입을 총 수입이 많은 것부터 작은 순으로 출력하시오, 연간 총수입은 월급에 12를 곱한 후 $100의 상여금을 더해서 계산하시오.
-select ename, sal, sal * 12 + 100
+select ename, sal, sal * 12 + 100 as yearSal
 from emp
-order by sal desc;
+order by yearSal desc;
 
 -- 3. 급여가 2000을 넘는 사원의 이름과 급여를 표현, 급여가 많은 것부터 작은 순으로 출력하시오.
 select ename, sal
