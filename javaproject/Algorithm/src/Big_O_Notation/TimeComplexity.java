8df50d8f76bf65507491e4b8d21980a4d33ec18e
jinsol1997 <mngjs97@gmail.com>
2022-10-14 18:02:56 +0900

    1014_1800

---
+++ javaproject/Algorithm/src/Big_O_Notation/TimeComplexity.java
@@ -0,0 +1,15 @@
+package Big_O_Notation;
+
+public class TimeComplexity {
+
+	public static void main(String[] args) {
+
+		// 빅오표기법 : 주어진 식을 값이 가장 큰 대표 항만 남겨서 나타내는 방법
+		// 시간복잡도 : 입력의 크기와 문제를 해결하는데 걸리는 시간의 상관관계
+		
+		// O(1) 상수시간 < O(lgN) 로그시간 < O(N) 선형시간 < O(NlgN) 다항시간 < O(N^) 다항시간 < O(2N^) < O(N!) 
+		
+		
+	}
+
+}
